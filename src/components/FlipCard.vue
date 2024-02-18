c846ac9b92f58ba001169c983a762a80ed07de9d
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-02-19 00:23:47 +0300

    Some improvment at FlipCard component

--- src/components/FlipCard.vue
+++ src/components/FlipCard.vue
@@ -2,7 +2,7 @@
   <div class="flip-card">
     <div class="flip-card-inner">
       <div class="flip-card-front">
-        <h1 class="rating">1</h1>
+        <h1 class="rank-number">1</h1>
         <img
           src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/jHxCeXnSchAuwHnmVatTgqMYdX8.jpg"
           alt="Poster"
@@ -10,9 +10,15 @@
         />
       </div>
       <div class="flip-card-back">
-        <h1>John Doe</h1>
-        <p>Architect & Engineer</p>
-        <p>We love that guy</p>
+        <h1 class="movie-title">The Wolf of Wall Street (1999)</h1>
+        <div class="overview">
+          <h3 class="header">Overview</h3>
+          <p class="body">
+            A New York stockbroker refuses to cooperate in a large securities
+            fraud case involving corruption on Wall Street, corporate banking
+            world and mob infiltration. Based on Jordan Belfort's autobiography.
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -23,7 +29,6 @@ import { ref } from "vue";
 <style scoped>
 .flip-card {
   background-color: transparent;
-
   width: 300px;
   height: 450px;
   perspective: 1000px;
@@ -38,13 +43,6 @@ import { ref } from "vue";
   transition: transform 0.6s;
   transform-style: preserve-3d;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-}
-.rating {
-  position: absolute;
-  top: 40%;
-  left: 50%;
-  color: white;
-  font-size: 4rem;
 }
 
 .flip-card:hover .flip-card-inner {
@@ -66,8 +64,28 @@ import { ref } from "vue";
 }
 
 .flip-card-back {
-  background-color: #2980b9;
-  color: white;
+  background-color: #242933;
+  color: #ffffff;
   transform: rotateY(180deg);
+}
+.rank-number {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  color: white;
+  font-size: 4rem;
+}
+.movie-title {
+  margin-top: 1rem;
+}
+.overview .header {
+  display: inline-block;
+  font-weight: 500;
+  border-bottom: solid 2px #eaeaea;
+}
+
+.overview .body {
+  font-size: 0.8rem;
+  margin: auto 1.2rem;
 }
 </style>
