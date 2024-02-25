5714cd0636fcb8d036f7882a324a9901e9d95fd9
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-02-26 00:06:26 +0300

    Style the movie card in the AddMovie.vue

--- src/pages/AddMovie.vue
+++ src/pages/AddMovie.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="getBack">
+  <div class="get-back">
     <base-button btnStyle="light" @click="getBack">
       <font-awesome-icon
         icon="fa-solid fa-left-long"
-        class="backLogo"
+        class="back-logo"
         size="xl"
       />
       Get Back
@@ -12,11 +12,66 @@
 
   <div class="container">
     <h1>Search Result of "{{ movieName }}"</h1>
-    <section class="resultSection">
-      <h4 class="noResult">No Movie Found</h4>
-      <div class="result-item">
-        <div class="moviePoster"></div>
-        <div class="movieInfo"></div>
+    <section class="result-section">
+      <h4 v-if="false" class="no-result">No Movie Found</h4>
+      <div else class="search-results">
+        <div class="result-item">
+          <div class="moviePoster">
+            <img
+              class="movie-poster"
+              src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/34m2tygAYBGqA9MXKhRDtzYd4MR.jpg"
+              alt="poster"
+            />
+          </div>
+          <div class="movie-info">
+            <p>
+              <span class="info-key">Title:</span>
+              <span class="title"> The wolf wall Street</span>
+            </p>
+            <p>
+              <span class="info-key">Release Date:</span>
+              <span class="release-date"> 2013-12-5</span>
+            </p>
+            <p>
+              <span class="info-key">Overview:</span>
+              <span class="overview">
+                A New York stockbroker refuses to cooperate in a large
+                securities fraud case involving corruption on Wall Street,
+                corporate banking world and mob infiltration. Based on Jordan
+                Belfort's autobiography.</span
+              >
+            </p>
+          </div>
+        </div>
+        <!-- Second Item -->
+        <div class="result-item">
+          <div class="moviePoster">
+            <img
+              class="movie-poster"
+              src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/34m2tygAYBGqA9MXKhRDtzYd4MR.jpg"
+              alt="poster"
+            />
+          </div>
+          <div class="movie-info">
+            <p>
+              <span class="info-key">Title:</span>
+              <span class="title"> The wolf wall Street</span>
+            </p>
+            <p>
+              <span class="info-key">Release Date:</span>
+              <span class="release-date"> 2013-12-5</span>
+            </p>
+            <p>
+              <span class="info-key">Overview:</span>
+              <span class="overview">
+                A New York stockbroker refuses to cooperate in a large
+                securities fraud case involving corruption on Wall Street,
+                corporate banking world and mob infiltration. Based on Jordan
+                Belfort's autobiography.</span
+              >
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   </div>
@@ -50,35 +105,92 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.backLogo {
-  color: #252a34;
-}
-.getBack {
-  display: flex;
-  position: sticky;
-  top: 1rem;
-  left: 1rem;
-}
 .container {
   width: 100%;
   margin: 1rem auto;
   text-align: center;
 }
+.back-logo {
+  color: #252a34;
+}
+.get-back {
+  display: flex;
+  position: sticky;
+  top: 1rem;
+  left: 1rem;
+}
+
 .container h1 {
   margin-bottom: 3rem;
 }
-.noResult {
+.no-result {
   color: #ff2e63;
+}
+/* Search Results */
+
+.result-item {
+  background-color: #d9d9d9;
+  border: 1px solid #7a7f89;
+  border-radius: 8px;
+  display: flex;
+  gap: 1rem;
+  padding: 1rem;
+  box-shadow: 0 0 7px rgba(37, 42, 52, 0.4);
+  width: 75%;
+  margin: 1rem auto;
+}
+.movie-poster {
+  width: 120px;
+  height: 180px;
+}
+.movie-info {
+  text-align: left;
+}
+.movie-info .info-key {
+  color: #252a34;
+  font-weight: bold;
+}
+.movie-info .title {
+  color: #ff2e63;
+  font-weight: bold;
+}
+.movie-info .release-date {
+  font-weight: bold;
+  color: #474c56;
+}
+.movie-info .overview {
+  font-size: 0.9rem;
+  color: #252a34;
 }
 
 @media only screen and (max-width: 768px) {
   .container h1 {
     font-size: 2rem;
   }
+  .result-item {
+    width: 100%;
+  }
 }
 @media only screen and (max-width: 481px) {
+  .container {
+    margin: 0;
+  }
   .container h1 {
     font-size: 1.25rem;
+  }
+
+  .movie-poster {
+    width: 90px;
+    height: 135px;
+  }
+  .movie-info .overview {
+    font-size: 0.8rem;
+  }
+  .result-item {
+    width: auto;
+    position: relative;
+    display: flex;
+    align-items: center;
   }
 }
 </style>
